81ba88e4070fdccdf3448f9a2c34753b2c853502
kmurata798 <kmurata798@gmail.com>
2020-10-28 22:11:04 -0700

    [Add] sorting_iterative.py -> is_sorted() bubble_sort() selectionSort()

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -3,28 +3,49 @@
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
+    Running time: O(n) because we are using a loop to traverse through each item in the list
+    Memory usage: O(1) because we aren't creating any additional data structures in the function"""
+    # Check that all adjacent items are in order, return early if so
+    for i in range(len(items) - 1):
+        current = items[i]
+        nextItem = items[i + 1]
+        if nextItem < current:
+            return False
+    return True
 
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    Running time: O(n^2) because we are using a nested for loop to traverse through the list
+    Memory usage: O(1) because we aren't creating any additional data structures in the function"""
+    # Repeat until all items are in sorted order
+
+    # loop through each number
+    for i in range(len(items):
+        for j in range(len(items) - 1):
+            if items[j] > items[j+1]:
+                # swap adjacent items that are out of order
+                temp = items[j]
+                items[j] = items[j+1]
+                items[j+1] = temp
 
 
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    O(n^2) because we are using a nested for loop to traverse through the list
+    Memory usage: O(1) because we aren't creating any additional data structures in the function"""
+    # Repeat until all items are in sorted order
+    for i in range(len(items) - 1):
+        # Find minimum item in unsorted items
+        smallestIndex = i
+        # loop through each UNSORTED item to find the next smallest number
+        for j in range(i + 1, len(items) - 1):
+            if items[j] < items[i]:
+                smallestIndex = j
+        # Swap it with first unsorted item
+        items[i], items[smallestIndex] = items[smallestIndex], items[i]
 
 
 def insertion_sort(items):
